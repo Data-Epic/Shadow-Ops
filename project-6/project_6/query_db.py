205270499028db18ec2ad95f24f23eef9ad4eb5a
GbotemiB <gbotemibolarinwa@gmail.com>
2023-12-14 19:47:10 +0100

    revised implementation based on review

--- project-6/project_6/query_db.py
+++ project-6/project_6/query_db.py
@@ -6,14 +6,14 @@ from dotenv import load_dotenv
 import logging
 
 # import ORM class from main
-from main import Artwork
+from main import ArtWork
 
 # load environment variables
 load_dotenv()
 
-username = os.environ.get("username")
-password = os.environ.get("password")
-host = os.environ.get("host")
+username = os.environ.get("USER")
+password = os.environ.get("PASSWORD")
+host = os.environ.get("HOST")
 
 # prepare connection to db
 engine = create_engine(f"postgresql+psycopg2://{username}:{password}@{host}")
@@ -22,31 +22,31 @@ Base.metadata.create_all(bind=engine)
 
 # total records in database
 with Session(bind=engine) as session:
-    query = session.query(Artwork).count()
+    query = session.query(ArtWork).count()
     logging.info(f"number of records is : {query}")
 
 # top 5 nationalities in database
 with Session(bind=engine) as session:
-    query = session.query(Artwork.Nationality, func.count(
-        Artwork.Nationality)).group_by(Artwork.Nationality).order_by(
-            func.count(Artwork.Nationality).desc())
-    logging.info(f"Top 5 Nationality : {query.all()[:5]}")
+    query = session.query(ArtWork.nationality, func.count(
+        ArtWork.nationality)).group_by(ArtWork.nationality).order_by(
+            func.count(ArtWork.nationality).desc())
+    logging.info(f"Top 5 Nationalities : {query.all()[:5]}")
 
 # group by counts for departments
 with Session(bind=engine) as session:
-    query = session.query(Artwork.Department, func.count(
-        Artwork.Department)).group_by(Artwork.Department).order_by(
-            func.count(Artwork.Department).desc())
+    query = session.query(ArtWork.department, func.count(
+        ArtWork.department)).group_by(ArtWork.department).order_by(
+            func.count(ArtWork.department).desc())
     logging.info(f"Departments : {query.all()}")
 
 # top 5 medium used in the database
 with Session(bind=engine) as session:
-    query = session.query(Artwork.Medium, func.count(Artwork.Medium)).group_by(
-        Artwork.Medium).order_by(func.count(Artwork.Medium).desc())
+    query = session.query(ArtWork.medium, func.count(ArtWork.medium)).group_by(
+        ArtWork.medium).order_by(func.count(ArtWork.medium).desc())
     logging.info(f"Top 5 Medium used : {query.all()[:5]}")
 
 # gender distribution in database
 with Session(bind=engine) as session:
-    query = session.query(Artwork.Gender, func.count(Artwork.Gender)).group_by(
-        Artwork.Gender).order_by(func.count(Artwork.Gender).desc())
+    query = session.query(ArtWork.gender, func.count(ArtWork.gender)).group_by(
+        ArtWork.gender).order_by(func.count(ArtWork.gender).desc())
     logging.info(f"Gender Distribution : {query.all()}")
